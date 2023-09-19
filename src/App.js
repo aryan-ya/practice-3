45e93f06797738b8b54d4f9cec686436cb2f105a
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-19 21:31:45 +0530

    react practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route,  Routes } from "react-router-dom";
+import Header from './components/Header';
+
 
 function App() {
   return (
     <Router>
-   
+   <Header></Header>
       <Routes>
         <Route />
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+ Drawer,
+ DrawerBody,
+ DrawerHeader,
+ DrawerOverlay,
+ DrawerContent,
+ DrawerCloseButton,
+ Button,
+
+}  from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+
+
+
+const Header = () => {
+  return (
+  <>
+  <div>
+    aryan
+  </div>
+  
+  </>
+  );
+  
+};
+
+export default Header;
